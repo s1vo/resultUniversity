35d6073b8793c67bb4f218fc7d7efb04bd4af1d1
s1vo <sivokonm@gmail.com>
2024-07-07 14:11:56 +0300

    Задание 3 и 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import '../index.css';
+import JS_IMAGE from '../assets/js.jpeg';
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const titleText = document.createElement('h1');
     titleText.innerHTML='I love JavaScript';
 
     const imgProgramming = document.createElement('img');
-    imgProgramming.src = './assets/js.jpeg';
+    imgProgramming.src = JS_IMAGE;
     imgProgramming.alt = 'Javascript logo';
 
     document.body.appendChild(titleText);
