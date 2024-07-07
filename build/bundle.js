0e43acfc699e55e7efb6a39c11877ec5517bbd3f
s1vo <sivokonm@gmail.com>
2024-07-07 13:42:37 +0300

    Задание 2

--- build/bundle.js
+++ build/bundle.js
@@ -2,6 +2,12 @@
 (function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 'use strict';
 
-for (var i = 0; i <= 100; i++) {
-  console.log("i: ".concat(i));
-}
+document.addEventListener('DOMContentLoaded', function () {
+  var titleText = document.createElement('h1');
+  titleText.innerHTML = 'I love JavaScript';
+  var imgProgramming = document.createElement('img');
+  imgProgramming.src = './assets/js.jpeg';
+  imgProgramming.alt = 'Javascript logo';
+  document.body.appendChild(titleText);
+  document.body.appendChild(imgProgramming);
+});
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-for(let i=0; i<=100; i++){
-    console.log(`i: ${i}`)
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const titleText = document.createElement('h1');
+    titleText.innerHTML='I love JavaScript';
+
+    const imgProgramming = document.createElement('img');
+    imgProgramming.src = './assets/js.jpeg';
+    imgProgramming.alt = 'Javascript logo';
+
+    document.body.appendChild(titleText);
+    document.body.appendChild(imgProgramming);
+
+});
